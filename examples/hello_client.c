884c8bd04b0330c63077df3b35dd6e12d8e8eca7
Logan Davis <logandavis1337@gmail.com>
2019-09-04 16:30:10 -0500

    Updated hello world code

--- examples/hello_client.c
+++ examples/hello_client.c
@@ -1,22 +1,42 @@
+// 0MQ Hello World Client
 #include <zmq.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-#include "zhelpers.h"
+//#include "zhelpers.h"
 
 int main(void) {
+    // Create Connection
     void* context = zmq_ctx_new();
     void* requester = zmq_socket(context, ZMQ_REQ);
-
     zmq_connect(requester, "tcp://localhost:5555");
 
+    // Message to send
+    char* message = "Hello, World!";
+
+    // Send 10 messages
     int request_nbr;
     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-        char* buffer = "SEND ME";
-        s_send(requester, buffer);
-        char* response = s_recv(requester);
-        printf("%s\n", response);
-        free(response);
+        char buff[256];
+        
+        // Send message wait for response
+        int sent = zmq_send(requester, message, strlen(message), 0);
+        if(sent == -1) {
+            printf("F IN CHAT\n");
+            return -1;
+        }
+
+        printf("Sent: %s\n", message);
+
+        // Receive Message and print
+        int returned = zmq_recv(requester, buff, 255, 0);
+        if(returned == -1) {
+            printf("FAIELD\n");
+            return -1;
+        }
+        buff[returned] = '\0';
+
+        printf("Received: %s\n", buff);
     }
     return 0;
 }
--- examples/hello_server.c
+++ examples/hello_server.c
@@ -1,25 +1,44 @@
+// 0MQ Hello World Server
 #include <zmq.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 
-#include "zhelpers.h"
+//#include "zhelpers.h"
 
 int main(void) {
-    printf("%d\n", EAGAIN);
-
+    // Create Server
     void* context = zmq_ctx_new();
     void* responder = zmq_socket(context, ZMQ_REP);
     int rc = zmq_bind(responder, "tcp://*:5555");
 
+    char* response = "Your Welcome...";
+
     while(1) {
-        char* str_recv = s_recv(responder);
-        if(str_recv == NULL) {
-            printf("The error message is: %s\n", strerror(errno));
+        char buff[256];
+
+        // Receive Request
+        int received = zmq_recv(responder, buff, 255, 0);
+        if(received == -1) {
+            printf("Something up with receiving\n");
             return -1;
         }
-        printf("%s\n", str_recv);
-        free(str_recv);
+        buff[received] = '\0';
+
+        printf("Received: %s\n", buff);
+
+        // "Do some work"
+        sleep(1);
+
+        // Send Response
+        int sent = zmq_send(responder, response, strlen(response), 0);
+        if(sent == -1) {
+            printf("Something up with shipping\n");
+            return -1;
+        }
+
+        printf("Sent: %s\n", response);
     }
 
     return 0;
